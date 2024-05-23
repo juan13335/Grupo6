2f430169b274885434fe8a8a8235036e9e21e60c
Jozze <jozzezalazar@gmail.com>
2024-05-23 16:06:09 -0300

    separo script

--- js/script.js
+++ js/script.js
@@ -27,58 +27,58 @@ showSlides();
 setInterval(nextSlide, 3000); // Cambia la imagen cada 3 segundos
 
 // contacto.html
-console.log('inicio')
-function validarForm(){
-  let nombre = document.querySelector('#nombre').value;
-  let apellido = document.querySelector('#apellido').value;
-  let email = document.querySelector('#email').value;
+// console.log('inicio')
+// function validarForm(){
+//   let nombre = document.querySelector('#nombre').value;
+//   let apellido = document.querySelector('#apellido').value;
+//   let email = document.querySelector('#email').value;
 
-  let soloPalabras = /^[a-z\s]+$/i; // Expresion regular
+//   let soloPalabras = /^[a-z\s]+$/i; // Expresion regular
 
-  let correo= /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+//   let correo= /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 
-  let vale=true //prueba  
-  let msgError= 'Datos Incorrecto:  ';
+//   let vale=true //prueba  
+//   let msgError= 'Datos Incorrecto:  ';
 
-//    Campos de texto
-  if(nombre === ''){
-    if (!soloPalabras.test(nombre)) {
-    vale=false; 
-    msgError += 'nombre'
+// //    Campos de texto
+//   if(nombre === ''){
+//     if (!soloPalabras.test(nombre)) {
+//     vale=false; 
+//     msgError += 'nombre'
     
-  }  
-  if(apellido === ""){
-    if (!soloPalabras.test(apellido)) {
-    vale=false; 
-    msgError += 'apellido'
+//   }  
+//   if(apellido === ""){
+//     if (!soloPalabras.test(apellido)) {
+//     vale=false; 
+//     msgError += 'apellido'
   
-  }
+//   }
 
-  if(email === ""){
-    if (!correo.test(email)) {
-    vale=false; 
-    msgError += 'apellido'
+//   if(email === ""){
+//     if (!correo.test(email)) {
+//     vale=false; 
+//     msgError += 'apellido'
   
-  }
+//   }
 
-  if ((vale)){
-    console.log('Datos ingresados!!')
-    }else{
-    console.log(msgError)
-    }   
-  }
-}
-}
-}
-// querySelector  
+//   if ((vale)){
+//     console.log('Datos ingresados!!')
+//     }else{
+//     console.log(msgError)
+//     }   
+//   }
+// }
+// }
+// }
+// // querySelector  
 
-const formulario = document.querySelector('button');
-let mensaje = document.querySelector('#rtaForm');
+// const formulario = document.querySelector('button');
+// let mensaje = document.querySelector('#rtaForm');
 
-formulario.addEventListener('click', evento => {
-  validarForm(); 
-  evento.preventDefault();
-})
+// formulario.addEventListener('click', evento => {
+//   validarForm(); 
+//   evento.preventDefault();
+// })
 // console.log('fin');
 
 
